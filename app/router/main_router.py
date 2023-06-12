877e468f59cd9d468c2ea95e4f4dcdcfc04f85e9
gamicolo <guillermoamicolo@gmail.com>
2023-06-12 10:52:02 -0300

    feat:add lists to main router

--- app/router/main_router.py
+++ app/router/main_router.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
-from app.router import elections
+from app.router import elections, lists
 
 router = APIRouter(prefix="")
 
 # Add all rotuers
 router.include_router(elections.router)
+router.include_router(lists.router)
 
 @router.get("/")
 def root():
