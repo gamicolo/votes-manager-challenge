beec20ad56f10cabdf1c39c2b1082c3156eb47cb
gamicolo <guillermoamicolo@gmail.com>
2023-06-12 12:13:31 -0300

    feat:remove json body validation

--- app/router/lists.py
+++ app/router/lists.py
@@ -8,10 +8,10 @@ from app.dependencies import get_db, db_session
 router = APIRouter(prefix="/lists", tags=["/lists"])
 
 @router.post("/")
-def create_list(request: Request, db: Session = Depends(get_db)):
+async def create_list(request: Request, db: Session = Depends(get_db)):
     db_session.set(db)
     try:
-        request_json = request.json()
+        request_json = await request.json()
     except:
         raise HTTPException(status_code=422, detail="Missing parameters on request body or its imcomplete")
 
@@ -29,15 +29,7 @@ def create_list(request: Request, db: Session = Depends(get_db)):
 def get_all_lists(election_id: int, db: Session = Depends(get_db)):
 
     db_session.set(db)
-    try:
-        request_json = request.json()
-    except:
-        raise HTTPException(status_code=422, detail="Missing parameters on request body or its imcomplete")
 
-    if not (request_json and 'election_id' in request_json and request_json['election_id']):
-        raise HTTPException(status_code=422, detail="Missing parameter 'election_id' on request body or its imcomplete")
-    election_id = request_json['election_id']
-    
     return lists_service.get_all_lists_from_db(election_id)
 
 @router.put("/{election_id}")
@@ -53,8 +45,8 @@ def update_(election_id: int, request: Request, db: Session = Depends(get_db)):
         raise HTTPException(status_code=422, detail="Missing parameter 'name' on request body or its imcomplete")
     list_name = request_json['name']
 
-    if not (request_json and 'election_id' in request_json and request_json['election_id']):
-        raise HTTPException(status_code=422, detail="Missing parameter 'election_id' on request body or its imcomplete")
-    election_id = request_json['election_id']
+#    if not (request_json and 'election_id' in request_json and request_json['election_id']):
+#        raise HTTPException(status_code=422, detail="Missing parameter 'election_id' on request body or its imcomplete")
+#    election_id = request_json['election_id']
 
     return lists_service.update_name_in_db(election_id, list_name)
