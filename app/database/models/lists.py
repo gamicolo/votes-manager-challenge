4afa2dda44d426bb31dbbb46ca8e5464cf6edee1
gamicolo <guillermoamicolo@gmail.com>
2023-06-12 22:32:04 -0300

    feat:remove unique from list name

--- app/database/models/lists.py
+++ app/database/models/lists.py
@@ -5,5 +5,5 @@ from app.database.dbbase import Base
 class Lists(Base):
     __tablename__ = "lists"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String, index=True)#TODO el nombre puede repetirse, lo que no se puede repetir es el id de la lista
     election_id = Column(Integer, ForeignKey("elections.id"))
