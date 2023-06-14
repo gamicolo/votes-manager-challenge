f62c2746deb2f8849f390e7c91a100b8f0a5becb
gamicolo <guillermoamicolo@gmail.com>
2023-06-13 21:25:11 -0300

    feat:remove comments

--- app/database/models/votes_result.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from app.database.dbbase import Base
-
-class Results(Base):
-    __tablename__ = "results"
-    id = Column(Integer, primary_key=True, index=True)
-    election_id = Column(Integer, ForeignKey("elections.id"))
-    list_id = Column(Integer, ForeignKey("list.id"))
-    seats_assigned = Column(Integer)
