fb9f3af35581ea4b7a3e6dbcdd38f8baeba250b9
gamicolo <guillermoamicolo@gmail.com>
2023-06-16 00:03:27 -0300

    feat:remove comments and unused packages

--- app/database/models/lists.py
+++ app/database/models/lists.py
@@ -1,9 +1,8 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 from app.database.dbbase import Base
 
 class Lists(Base):
     __tablename__ = "lists"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)#TODO el nombre puede repetirse, lo que no se puede repetir es el id de la lista
+    name = Column(String, index=True)
     election_id = Column(Integer, ForeignKey("elections.id"))
--- app/database/schemas/votes.py
+++ app/database/schemas/votes.py
@@ -1,7 +1,4 @@
 from pydantic import BaseModel
-#from datetime import datetime
-#from app.schemas.elections import Elections
-#from app.schemas.lists import Lists
 
 class Votes(BaseModel):
     id: int
