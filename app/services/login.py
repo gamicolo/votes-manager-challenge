b89749cf42545fd78bd54de5cda101f5f43eaa17
gamicolo <guillermoamicolo@gmail.com>
2023-06-16 09:26:46 -0300

    feat:change get_user function to return a valid UserInDB pydantic base model and remove comments

--- app/services/login.py
+++ app/services/login.py
@@ -31,12 +31,18 @@ def get_user(db: Session, username: str):
     """
     Get the user from the DB
     """
-
     user_from_db = db.query(UserModel).filter(UserModel.username == username).first()
 
     if user_from_db:
-        return UserInDB(**user_from_db)
+        user = UserInDB(
+                username=user_from_db.username,
+                full_name=user_from_db.full_name,
+                email=user_from_db.email,
+                hashed_password=user_from_db.hashed_password,
+                disable=user_from_db.disable
+               )
 
+        return user
     return None
 
 def authenticate_user(db: Session, username: str, password: str):
@@ -86,33 +92,3 @@ async def get_current_active_user(
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
-#def create_list_votes_in_db(election_id: int, list_name: str, votes: int):
-#
-#    try:
-#        #the create method of list_crud instance validates the existance of the election_id and list_name before create the list
-#        list_votes_created = votes_crud.create(election_id, list_name, votes)
-#    except AlreadyExistsOnDBException as error:
-#        raise HTTPException(status_code=409, detail=f"The list with the name {list_name} for the election with id {election_id} already exist on the system")
-#
-#    return list_votes_created
-#
-#def get_all_votes_from_db(election_id: int):
-#
-#    try:
-#        votes_in_db = votes_crud.get_all_votes(election_id)
-#    except NotFoundOnDBException as error:
-#        raise HTTPException(status_code=404, detail=f"No votes for the id {election_id} were found on the system")
-#
-#    return votes_in_db
-#
-##TODO evalur si quitar esta funcion (no se realizaran actualizaciones de votos)
-#def update_votes_in_db(election_id: int, list_name: str, votes: int):
-#
-#    try:
-#        list_votes_updated = votes_crud.update(election_id, list_name, votes)
-#    except AlreadyExistsOnDBException as error:
-#        raise HTTPException(status_code=409, detail=f"The list with the name {list_name} for the election with id {election_id} already exist on the system")
-#
-#    return list_votes_updated
-#
-#
