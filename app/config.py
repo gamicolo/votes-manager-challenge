97e6c6c4e1e2f4d791fc54837982878fde92ff4b
gamicolo <guillermoamicolo@gmail.com>
2023-06-16 00:08:12 -0300

    feat:add instances dependencies and configuration for login

--- app/config.py
+++ app/config.py
@@ -5,4 +5,8 @@ class Settings(BaseSettings):
     DATABASE_URL = "sqlite:///./votes-manager.db"
     DB_LIST_LIMIT = 50
 
+    SECRET_KEY = "78b5f6caeb24de304595a40a591953a615a6e3513e1e9721c1d6290840afc5db"
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 settings = Settings()
--- app/dependencies.py
+++ app/dependencies.py
@@ -1,8 +1,8 @@
 from app.database.dbbase import SessionLocal
 from contextvars import ContextVar
 from sqlalchemy.orm import Session
-
-import requests
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
 def get_db():
     db = SessionLocal()
@@ -13,3 +13,7 @@ def get_db():
 
 
 db_session: ContextVar[Session] = ContextVar('db_session')
+
+#Instances for login endpoint
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
