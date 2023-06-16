ac44adae1fae9ed537eddb9bffcec6b8c92c8cff
gamicolo <guillermoamicolo@gmail.com>
2023-06-16 00:06:11 -0300

    feat:add login endpoint to main_route

--- app/router/main_router.py
+++ app/router/main_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
-from app.router import elections, lists, votes
+from app.router import elections, lists, votes, login
 
 router = APIRouter(prefix="")
 
@@ -8,6 +8,7 @@ router = APIRouter(prefix="")
 router.include_router(elections.router)
 router.include_router(lists.router)
 router.include_router(votes.router)
+router.include_router(login.router)
 
 @router.get("/")
 def root():
